f8d28b0e9807038ceb78b3953163b7d1f8093247
Sam Alexander <sam@stacked.co.nz>
2016-07-01 15:29:31 +1200

    prepended formattedName and formattedRole

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,8 @@
 var name = "Samuel Alexander";
 var formattedName = HTMLheaderName.replace("%data%", name);
 
-$("#header").append(formattedName);
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
