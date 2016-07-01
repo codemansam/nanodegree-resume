630530f28aa90cedb365ca7d3c1ecdf064b41a6a
Sam Alexander <sam@stacked.co.nz>
2016-07-01 16:50:23 +1200

    added work and education objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,3 +6,50 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var bio = {
+    "name": "Samuel Alexander",
+    "role": "Web Developer",
+    "contacts": {
+        "mob": "022 656 7919",
+        "email": "same@stacked.co.nz",
+        "github": "https://github.com/codemansam",
+        "location": "Auckland, NZ"
+    },
+    "bioPic": "images.fry.jpg",
+    "welcomeMessage": "Hi there!",
+    "skills": ["awesomeness", "studying", "manualism", "twaddling"]
+}
+$("#main").append(bio.skills);
+
+var work = {};
+
+work.position = "Lawn Mowing Contractor";
+work.employer = "Self";
+work.years = "6";
+work.city ="Auckland";
+
+var education = {};
+education["name"] = "Auckland University"
+education["years"] = "1998-2002";
+education["city"] = "Auckland, NZ";
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
+var education = {
+    "schools": [
+    {
+        "name": "Auckland University",
+        "city": "Auckland, NZ",
+        "degree": "BSc",
+        "major": "Geography"
+    },
+    {
+        "name": "Auckland University",
+        "city": "Auckland, NZ",
+        "degree": "Ba",
+        "major": "Philosopy"
+    }
+    ]
+};
