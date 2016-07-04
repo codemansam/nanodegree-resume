977770133d5496af33ece4ff105651bc13e8c939
Sam Alexander <sam@stacked.co.nz>
2016-07-04 21:15:10 +1200

    Beautified html css and javascript files

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 /**
-* @description Stores biographic information.
-*/
+ * @description Stores biographic information.
+ */
 var bio = {
     "name": "Samuel Alexander",
     "role": "Web Developer",
@@ -14,18 +14,18 @@ var bio = {
     "skills": ["Impressions", "Studying", "Manualism", "Twaddling"],
     "biopic": "images/kitty.jpg",
 
-/**
-* @description This function takes bio information, applies helper.js formatting and
-*  attaches it to the relevant HTML section.
-*/
-display: function() {
+    /**
+     * @description This function takes bio information, applies helper.js formatting and
+     *  attaches it to the relevant HTML section.
+     */
+    display: function() {
 
-    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mob);
-    var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+        var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mob);
+        var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
-    var idStrings = ["#topContacts", "#footerContacts"];
+        var idStrings = ["#topContacts", "#footerContacts"];
         for (var i = 0, id; i < idStrings.length; i++) {
             id = idStrings[i];
 
@@ -33,37 +33,36 @@ display: function() {
             $(id).append(formattedEmail);
             $(id).append(formattedGithub);
             $(id).append(formattedLocation);
-            }
+        }
 
 
 
-    var formattedName = HTMLheaderName.replace("%data%",bio.name);
-    var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-    var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-    var formattedPic  = HTMLbioPic.replace("%data%", bio.biopic);
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
 
-    $("#header").prepend(formattedRole);
-    $("#header").prepend(formattedName);
-    $("#header").append(formattedWelcomeMessage);
-    $("#header").append(formattedPic);
+        $("#header").prepend(formattedRole);
+        $("#header").prepend(formattedName);
+        $("#header").append(formattedWelcomeMessage);
+        $("#header").append(formattedPic);
 
 
 
-    $("#header").append(HTMLskillsStart);
-    for (i = 0; i < bio.skills.length; i++) {
+        $("#header").append(HTMLskillsStart);
+        for (i = 0; i < bio.skills.length; i++) {
 
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-        $("#header").append(formattedSkill);
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+            $("#header").append(formattedSkill);
         }
     }
 };
 
 /**
-* @description Stores an array of tertiary education information and online courses.
-*/
+ * @description Stores an array of tertiary education information and online courses.
+ */
 var education = {
-    "schools": [
-    {
+    "schools": [{
         "name": "Auckland University",
         "location": "22 Princes Street, Auckland, NZ",
         "degree": "BSc",
@@ -71,46 +70,43 @@ var education = {
         "dates": "2002",
         "url": "https://www.auckland.ac.nz/en.html"
 
-    },
-    {
+    }, {
         "name": "Auckland University",
         "location": "22 Princes Street, Auckland, NZ",
         "degree": "BA",
         "majors": ["Philosophy (Ethics)"],
         "dates": "2002",
         "url": "https://www.auckland.ac.nz/en.html"
-    }
-    ],
-    "onlineCourses": [
-        {
+    }],
+    "onlineCourses": [{
         "title": "Javascript Crash Course",
         "school": "Udacity",
         "dates": "2016",
         "url": "http://www.udacity.com/course/ud804"
-        }
-    ],
+    }],
 
-/**This function takes education information, applies formatting from helper.js and
- * attaches it to the relevant HTML section.
- */
+    /**
+     * @function Takes education information, applies formatting from helper.js and
+     * attaches it to the relevant HTML section.
+     */
     display: function() {
-    if (education.schools.length > 0) {
+        if (education.schools.length > 0) {
 
-        for (var school = 0; school < education.schools.length; school++) {
-            $("#education").append(HTMLschoolStart);
+            for (var school = 0; school < education.schools.length; school++) {
+                $("#education").append(HTMLschoolStart);
 
-            var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
-            var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-            $(".education-entry:last").append(formattedSchoolName + " " + formattedDegree);
+                var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+                var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+                $(".education-entry:last").append(formattedSchoolName + " " + formattedDegree);
 
-            var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-            $(".education-entry:last").append(formattedDates);
+                var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+                $(".education-entry:last").append(formattedDates);
 
-            var formattedCity = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-            $(".education-entry:last").append(formattedCity);
+                var formattedCity = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+                $(".education-entry:last").append(formattedCity);
 
-            var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
-            $(".education-entry:last").append(formattedMajor);
+                var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+                $(".education-entry:last").append(formattedMajor);
             }
 
             for (var course = 0; course < education.onlineCourses.length; course++) {
@@ -131,100 +127,95 @@ var education = {
     }
 };
 /**
-* @description Stores an array of jobs with the corresponding work details.
-*/
+ * @description Stores an array of jobs with the corresponding work details.
+ */
 var work = {
-    "jobs": [
-        {
-            "employer": "North Shore Lawns",
-            "title": "'Boss'",
-            "location": "6 Sispara Place, Beach Haven, Auckland, NZ",
-            "dates": "2009 - 2015",
-            "description": "Lawn mowing contractor."
-        },
-        {
-            "employer": "NZ Post Inc",
-            "title": "'Postie'",
-            "location": "76 Porana Rd, Hillcrest, Auckland, NZ",
-            "dates": "2004 - 2009",
-            "description": "Responsible for sorting and delivering residential mail."
-        }
-    ],
+    "jobs": [{
+        "employer": "North Shore Lawns",
+        "title": "'Boss'",
+        "location": "6 Sispara Place, Beach Haven, Auckland, NZ",
+        "dates": "2009 - 2015",
+        "description": "Lawn mowing contractor."
+    }, {
+        "employer": "NZ Post Inc",
+        "title": "'Postie'",
+        "location": "76 Porana Rd, Hillcrest, Auckland, NZ",
+        "dates": "2004 - 2009",
+        "description": "Responsible for sorting and delivering residential mail."
+    }],
 
-/**
-* @description This function takes work history information, applies formatting from helper.js and
-* attaches it to the relevant HTML section.
-*/
+    /**
+     * @description This function takes work history information, applies formatting from helper.js and
+     * attaches it to the relevant HTML section.
+     */
     display: function() {
-    if (work.jobs.length > 0){
-        for (var job = 0; job < work.jobs.length; job++) {
-            $("#workExperience").append(HTMLworkStart);
+        if (work.jobs.length > 0) {
+            for (var job = 0; job < work.jobs.length; job++) {
+                $("#workExperience").append(HTMLworkStart);
 
-            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-            var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-            var formattedEmployerTitle = formattedEmployer + formattedWorkTitle;
+                var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+                var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+                var formattedEmployerTitle = formattedEmployer + formattedWorkTitle;
 
-            $(".work-entry:last").append(formattedEmployerTitle);
+                $(".work-entry:last").append(formattedEmployerTitle);
 
-            var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-            $(".work-entry:last").append(formattedDates);
+                var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+                $(".work-entry:last").append(formattedDates);
 
-            var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-            $(".work-entry:last").append(formattedDescription);
+                var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+                $(".work-entry:last").append(formattedDescription);
             }
         }
     }
 };
 /**
-* @description Stores an array of projects with the corresponding information.
-*/
+ * @description Stores an array of projects with the corresponding information.
+ */
 var projects = {
-    "projects": [
-        {
-            "title": "Fusion Power",
-            "dates": "2016",
-            "description": "Harness the power of the stars?  No problem, Sammy's solved it.",
-            "images": ["images/fusion3.jpg", "images/fusion1.jpg"]
-        },
-        {
-            "title": "Hyperloop",
-            "dates": "2014",
-            "description": "Cheap, reliable, super quick travel? Sorted!",
-            "images": ["images/hyperloop3.jpg", "images/hyperloop2.jpg"]
-        }
-    ],
+    "projects": [{
+        "title": "Fusion Power",
+        "dates": "2016",
+        "description": "Harness the power of the stars?  No problem, Sammy's solved it.",
+        "images": ["images/fusion3.jpg", "images/fusion1.jpg"]
+    }, {
+        "title": "Hyperloop",
+        "dates": "2014",
+        "description": "Cheap, reliable, super quick travel? Sorted!",
+        "images": ["images/hyperloop3.jpg", "images/hyperloop2.jpg"]
+    }],
 
-/**
-* @description This function takes project information, applies formatting from helper.js and
-* attaches it to the relevant HTML section.
-*/
+    /**
+     * @description This function takes project information, applies formatting from helper.js and
+     * attaches it to the relevant HTML section.
+     */
     display: function() {
-    if (projects.projects.length > 0) {
-        for (var project = 0; project < projects.projects.length; project++) {
-             $("#projects").append(HTMLprojectStart);
+        if (projects.projects.length > 0) {
+            for (var project = 0; project < projects.projects.length; project++) {
+                $("#projects").append(HTMLprojectStart);
 
-            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-            $(".project-entry:last").append(formattedProjectTitle);
+                var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+                $(".project-entry:last").append(formattedProjectTitle);
 
-            var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-            $(".project-entry:last").append(formattedProjectDates);
+                var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+                $(".project-entry:last").append(formattedProjectDates);
 
-            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-             $(".project-entry:last").append(formattedProjectDescription);
+                var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+                $(".project-entry:last").append(formattedProjectDescription);
 
-            if (projects.projects[project].images.length > 0) {
-                for (var image = 0; image < projects.projects[project].images.length; image++) {
-                    var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-                    $(".project-entry:last").append(formattedProjectImage);
+                if (projects.projects[project].images.length > 0) {
+                    for (var image = 0; image < projects.projects[project].images.length; image++) {
+                        var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                        $(".project-entry:last").append(formattedProjectImage);
                     }
                 }
             }
         }
     }
 };
+
 /**
-* @description The display methods are called here and the information is processed.
-*/
+ * @description The display methods are called here and the information is processed.
+ */
 bio.display();
 education.display();
 work.display();
@@ -234,5 +225,3 @@ $("#mapDiv").append(googleMap);
 
 
 //Google Maps API: "Browser Key 1": "AIzaSyAvoT7YGnNfHpMCDKzcBd31jO9CNeO5qws"
-
-
